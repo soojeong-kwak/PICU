2793d950781ecbefb29eeacddf73d27a6fb625c9
soojeong-kwak <168149910+soojeong-kwak@users.noreply.github.com>
2024-04-26 10:22:56 +0900

    Add files via upload
    R package list

---
+++ 99_packageList.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(magrittr)
+library(data.table)
+library(readxl)
+library(lubridate)
+library(pROC)
+library(jsonlite)
+
+library(gridExtra)   # grid plot
+library(Rlibeemd)    # EMD
+library(hht)         # hilbert transform
+library(gsignal)     # FFT
+library(IRISSeismic) # cross spectrum
+library(eseis)       # SNR
+library(wavelets)    # wavelets
+
+options(scipen = 99)
